a397ecbd2b809812845d55fa95dd63c8c4e08abf
daikins <dan.aikins@gmail.com>
2018-07-26 10:52:24 -0400

    Initial commit, copied OddOrEven.py

---
+++ OddOrEven/OddOrEvenExtras.py
@@ -0,0 +1,27 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+#
+# OddOrEvenExtras.py
+#
+# ######################################################
+#
+# Ask the user for a number. Depending on whether the number is even or odd,
+# print out an appropriate message to the user. Hint: how does an even / odd
+# number react differently when divided by 2?
+#
+# Extras:
+#
+# 1. If the number is a multiple of 4, print out a different message.
+# 2. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+#    If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+#
+# ######################################################
+
+print("Testing if a number is odd or even")
+user_input = int(input("Enter a number: "))
+
+if user_input % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
