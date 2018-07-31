3bbfe591bb3f152daae476e2f94aa23d8a21ca78
daikins <dan.aikins@gmail.com>
2018-07-31 11:54:24 -0400

    initial commit - comments

---
+++ StringLists_Palindrome.py
@@ -0,0 +1,16 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
+#
+# StringLists_Palindrome.py
+#
+# Exercise 6
+#
+# ######################################################
+#
+# Ask the user for a string and print out whether this string is a palindrome or not.
+# (A palindrome is a string that reads the same forwards and backwards.)
+#
+# ######################################################
+
+# Original Problem
