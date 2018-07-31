3a2d89a38c6b62a43d04df73d7666dc719c9994b
daikins <dan.aikins@gmail.com>
2018-07-31 16:34:54 -0400

    added header comment

---
+++ ListComprehensions.py
@@ -0,0 +1,20 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
+#
+# ListComprehensions.py
+#
+# Exercise 7
+#
+# ######################################################
+#
+# Let’s say I give you a list saved in a variable:
+#
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+#
+# Write one line of Python that takes this list a and makes a new list
+# that has only the even elements of this list in it.
+#
+# ######################################################
+
+# Original Problem
