1602386b9173b49128b5f071021a4b119effce66
daikins <dan.aikins@gmail.com>
2018-08-01 14:54:07 -0400

    attempt at randomly generating lists

--- ListOverlapComprehensions.py
+++ ListOverlapComprehensions.py
@@ -30,10 +30,17 @@
 # Randomly generate two lists to test this
 #
 # ######################################################
+import random
 
 # Original Problem
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+a = random.sample(range(1, 50), 10)
+b = random.sample(range(1, 50), 10)
+
+print("a: {}".format(a))
+print("b: {}".format(b))
 
 # Not the correct solution, skipping for now.
 # TODO: implement actual solution
