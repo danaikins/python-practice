fe1045ac5378102660f95a3380632ec58e4c64ba
daikins <dan.aikins@gmail.com>
2018-08-06 14:12:30 -0400

    header comment

---
+++ CheckPrimality.py
@@ -0,0 +1,20 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/04/10/10-list-overlap-comprehensions.html
+#
+# ListOverlapComprehensions.py
+#
+# Exercise 10
+#
+# ######################################################
+#
+# Ask the user for a number and determine whether the number is prime or not.
+# (For those who have forgotten, a prime number is a number that has no divisors.).
+# You can (and should!) use your answer to Exercise 4 to help you. Take this opportunity
+# to practice using functions, described below.
+#
+# Discussion
+#
+# ######################################################
+
+# Original Problem
