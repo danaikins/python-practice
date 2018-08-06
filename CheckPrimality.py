b68e9b1d0647b1b372bc8aff1cdfe479eb2d1c7e
daikins <dan.aikins@gmail.com>
2018-08-06 14:34:54 -0400

    Finished code

--- CheckPrimality.py
+++ CheckPrimality.py
@@ -18,3 +18,33 @@
 # ######################################################
 
 # Original Problem
+
+
+def get_integer(text="Enter an Integer: "):
+    return int(input(text))
+
+
+def check_prime(user_input):
+    list_divisors = find_divisors(user_input)
+    number_is_prime = len(list_divisors)
+
+    return number_is_prime == 0
+
+
+def find_divisors(user_input):
+    half_input = user_input // 2
+    numbers = range(2, half_input + 1)
+    answer = []
+    for i in numbers:
+        if user_input % i == 0:
+            answer.append(i)
+    return answer
+
+
+user_number = get_integer("Enter a number to be checked for primality: ")
+number_prime = check_prime(user_number)
+
+if number_prime:
+    print("The number \"{}\" is prime.".format(user_number))
+else:
+    print("The number \"{}\" is NOT prime.".format(user_number))
