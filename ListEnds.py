102104bc45b469f4c65b9bd1b16c9bad1db837c6
daikins <dan.aikins@gmail.com>
2018-08-07 14:34:36 -0400

    header comment

---
+++ ListEnds.py
@@ -0,0 +1,16 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
+#
+# ListEnds.py
+#
+# Exercise 12
+#
+# ######################################################
+#
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only
+# the first and last elements of the given list. For practice, write this code inside a function.
+#
+# ######################################################
+
+# Original Problem
