277c4969c24fbe17cc06c81b56428d9ab1e55f5a
danaikins <clK5p&e&HRcr6Urn0>
2018-08-17 19:35:25 -0400

    header commit

---
+++ DecodeAWebPage.py
@@ -0,0 +1,13 @@
+# ######################################################
+#
+# https://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html#
+# DecodeAWebPage.py
+#
+# Exercise 17
+#
+# ######################################################
+#
+# Use the BeautifulSoup and requests Python packages to print out a list of all
+# the article titles on the New York Times homepage.
+#
+# ######################################################
