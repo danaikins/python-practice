90d5101a1be32157630d296e98049e72975c289a
danaikins <clK5p&e&HRcr6Urn0>
2018-08-17 20:04:35 -0400

    added header, started a bit of code

--- DecodeAWebPage.py
+++ DecodeAWebPage.py
@@ -9,6 +9,11 @@
 # ######################################################
 #
 # Use the BeautifulSoup and requests Python packages to print out a list of all
-# the article titles on the New York Times homepage.
+# the article titles on the [New York Times](http://www.nytimes.com/) homepage.
 #
 # ######################################################
+import requests
+
+
+web_page = input("Enter URL to Parse: ")
+url = 'web_page'
