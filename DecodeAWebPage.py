fe850aea0b29a4df89b58a02e947d8331c05164d
daikins <dan.aikins@gmail.com>
2018-08-20 16:45:44 -0400

    exercise finished

--- DecodeAWebPage.py
+++ DecodeAWebPage.py
@@ -11,6 +11,15 @@
 # titles on the New York Times homepage.
 #
 # ######################################################
+import requests
+from bs4 import BeautifulSoup
 
-web_page = input("Enter URL to Parse: ")
-url = 'web_page'
+web_page = "https://www.nytimes.com"
+r = requests.get(web_page)
+r_html = r.text
+soup = BeautifulSoup(r_html, 'html.parser')
+for link in soup.find_all('h2'):
+    if link.contents[0].name == 'a':
+        headline = link.contents[0].string
+        if headline is not None:
+            print(headline)
